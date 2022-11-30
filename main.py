31ab1c6c6abe27e38adece8bf981168e14bb6984
nicole rivera <41299759+nicolerivera1@users.noreply.github.com>
2022-11-30 16:02:05 -0500

    add web page files

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-'''
+"""
 main program to execute FastApi and the data
-'''
+"""
 
 from typing import Optional
 
@@ -11,3 +11,30 @@ import requests
 import json
 
 app = FastAPI()
+
+file = "https://raw.githubusercontent.com/nicolerivera1/UdeA-papers-filtered/main/data/papers_filtered.json"
+
+
+@app.get("/")
+def read_item(author_name: str = "", orcid: str = ""):
+    """
+    This allows you to filter by author name or orcid
+    Make sure orcid includes the complete https direction
+    """
+    # Real time JSON file
+    r = requests.get(file)
+    db = r.json()
+
+    # choose between name and orcid
+
+    new_db = [d for d in db["authorship"] if d.get("display_name") == author_name]
+    f = open("data/filtered_by_author.json", "w")
+    json.dump(new_db, f)
+    f.close()
+    # with open(file) as json_file:
+    #   db=json.load(json_file)
+
+    if not author_name:
+        return db
+    else:
+        return new_db
